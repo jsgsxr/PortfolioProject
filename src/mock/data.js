cb1b4c5f48fb54dd8a2253701e275d643fc63ce2
Justin <53011686+jsgsxr@users.noreply.github.com>
2021-09-25 00:50:54 +0800

    Justin Specifics Changed

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,67 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Justin Smith | Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Justin Smith Portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Worldwide |',
+  name: 'Justin Smith',
+  subtitle: 'Web Developer',
+  cta: 'Know More',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile.jpeg',
+  paragraphOne:
+    'Experienced in HTML, CSS, JavaScript, React/Next.js, DART, Flutter, Node.js, Firebase, and quick to pick up any Object Oriented framework.',
+  paragraphTwo:
+    'Skilled in Electrical Troubleshooting, Management, Problem Solving, and Training. Strong arts and design professional, with a Bachelors Degree focused in Sound Design/Music Production from Full Sail University.',
+  paragraphThree:
+    'Skilled Sound Designer with a demonstrated history of working in the entertainment industry. ',
+  resume: 'https://pdfhost.io/v/o91VeK~7E_ResumeJustinSmith', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'HouseofMusic.png',
+    title: 'House of Music',
+    info: 'Social-Media-style design for testing backend and database knowledge using React/Next.js',
+    info2: 'Login, post, comment, and make friends with local musicians!',
+    url: 'https://house-of-music.vercel.app/',
+    repo: 'https://github.com/jsgsxr/house-of-music', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'sspappimg.png',
+    title: 'SilverSmith Productions Mobile APP',
+    info: 'Mobile app based on SilverSmith Productions marketing',
+    info2: 'Using Flutter and DART to produce mobile applications',
+    url: 'https://github.com/jsgsxr/SSP-APP-Build',
+    repo: 'https://github.com/jsgsxr/SSP-APP-Build', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'formimg.png',
+    title: 'Customer Satisfaction Form',
+    info: 'Form using only HTML and CSS to send data through API',
+    info2: 'Practical form knowledge and control using only HTML and CSS',
+    url: 'https://github.com/jsgsxr/customersatisfactionform',
+    repo: 'https://github.com/jsgsxr/customersatisfactionform', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Building dreams from around the world',
+  btn: 'Contact Me!',
+  email: 'silversmithpro@icloud.com',
 };
 
 // FOOTER DATA
@@ -67,28 +70,23 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'facebook',
+      url: 'https://www.facebook.com/SilverSmithProTaiwan',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/justin-smith-taiwan/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/jsgsxr',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Justin Smith Portfolio'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Justin Smith Portfolio'} />
       </Helmet>
       <App />
     </>
